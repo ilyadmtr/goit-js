bacea4b3dd9165ec32b6224502265ea70225980d
ilyadmtr <ilyadmytrenko@gmail.com>
2020-03-25 10:26:19 +0200

    added task-1 HMWRK-2

--- Homework-2/task-1.js
+++ Homework-2/task-1.js
@@ -1,21 +1,47 @@
 "use strict"
 
 //1
-let fruits = ["яблоки", "бананы", "апельсины"];
+// let fruits = ["яблоки", "бананы", "апельсины"];
 
-console.log(fruits);
-console.log(fruits[1]);
+// console.log(fruits);
+// console.log(fruits[1]);
 
-fruits[1] = "киви";
-console.log(fruits[1]);
+// fruits[1] = "киви";
+// console.log(fruits[1]);
 
-fruits[3] =  "груши";
-fruits[4] =  "виноград";
-fruits[5] =  "персики";
+// fruits[3] =  "груши";
+// fruits[4] =  "виноград";
+// fruits[5] =  "персики";
 
-console.log(fruits);
+// console.log(fruits);
 
 //2
-let test2 = [4, 12, 6, 16, 13, 8, 13, 4, 15, 11, 14, 10, 15, 12];
-for(score1 = 12; score1 < test2; i += 1){
-    console.log("students item:", students[i]);
+let test1 = [4, 12, 6, 16, 13, 8, 13, 4, 15, 11, 14, 10, 15, 12];
+// for (var score1 = 0; score1 < test1.length; score1++){
+//     if(test1[score1] > 12){
+//         console.log(test1[score1]);
+//     }
+// }
+
+//3
+let test2 = [17, 13, 4, 15, 10, 12, 16, 21, 13, 8, 14, 18, 15, 20];
+// for (var score2 = 0; score2 < test2.length; score2++){
+//     if(test2[score2] > 15){
+//         console.log(test2[score2]);
+//     }
+// }
+
+//4
+let test3 =[20, 12, 16, 22, 13, 8, 13, 4, 15, 10, 14, 18, 15, 22];
+// for (var score3 = 0; score3 < test3.length; score3++){
+//     if(test3[score3] > 16){
+//         console.log(test3[score3]);
+//     }
+// }
+
+//5
+// let tests = [test1,test2,test3];
+
+// for (let test of tests) {
+//     console.log( test );
+// }
