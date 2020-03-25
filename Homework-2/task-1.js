4f6f1b007a5b51d89fe1f26d4f823836857a91bb
ilyadmtr <ilyadmytrenko@gmail.com>
2020-03-25 15:09:18 +0200

    added task-1 HMWRK-2

--- Homework-2/task-1.js
+++ Homework-2/task-1.js
@@ -7,7 +7,7 @@
 // console.log(fruits[1]);
 
 // fruits[1] = "киви";
-// console.log(fruits[1]);
+//console.log(fruits);
 
 // fruits[3] =  "груши";
 // fruits[4] =  "виноград";
@@ -16,32 +16,45 @@
 // console.log(fruits);
 
 //2
-let test1 = [4, 12, 6, 16, 13, 8, 13, 4, 15, 11, 14, 10, 15, 12];
-// for (var score1 = 0; score1 < test1.length; score1++){
-//     if(test1[score1] > 12){
-//         console.log(test1[score1]);
+// let test1 = [4, 12, 6, 16, 13, 8, 13, 4, 15, 11, 14, 10, 15, 12];
+// const score1 = 12;
+
+// for (let i = 0; i < test1.length; i++){
+//     if(test1[i] <= score1){
+//         continue;
 //     }
+//     console.log(test1[i]);
 // }
 
 //3
-let test2 = [17, 13, 4, 15, 10, 12, 16, 21, 13, 8, 14, 18, 15, 20];
-// for (var score2 = 0; score2 < test2.length; score2++){
-//     if(test2[score2] > 15){
-//         console.log(test2[score2]);
+// let test2 = [17, 13, 4, 15, 10, 12, 16, 21, 13, 8, 14, 18, 15, 20];
+// const score2 = 15;
+
+// for (let k = 0; k < test2.length; k++){
+//     if(test2[k] <= score2){
+//         continue;
 //     }
+//     console.log(test2[k]);
 // }
 
 //4
-let test3 =[20, 12, 16, 22, 13, 8, 13, 4, 15, 10, 14, 18, 15, 22];
-// for (var score3 = 0; score3 < test3.length; score3++){
-//     if(test3[score3] > 16){
-//         console.log(test3[score3]);
+// let test3 =[20, 12, 16, 22, 13, 8, 13, 4, 15, 10, 14, 18, 15, 22];
+// const score3 = 16;
+
+// for (let j = 0; j < test3.length; j++){
+//     if(test3[j] > score3){
+//         continue;
 //     }
+//     console.log(test3[j]);
 // }
 
 //5
-// let tests = [test1,test2,test3];
+// let tests = [
+//     [4, 12, 6, 16, 13, 8, 13, 4, 15, 11, 14, 10, 15, 12],
+//     [17, 13, 4, 15, 10, 12, 16, 21, 13, 8, 14, 18, 15, 20],
+//     [20, 12, 16, 22, 13, 8, 13, 4, 15, 10, 14, 18, 15, 22]
+// ];
 
 // for (let test of tests) {
-//     console.log( test );
+//   console.log( test );
 // }
